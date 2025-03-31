8a6957e53c859da579077b69227058012aa6f8ce
Elijah <Elijah@gmail.com>
2025-03-31 20:51:58 +0300

    Update catalog api

--- catalogAPI/settings.py
+++ catalogAPI/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'catalogAPI.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'catalog_db_test',
+        'PASSWORD': 'root',
+        'USER': 'postgres',
+        'PORT': '5432',
+        'HOST': 'host.docker.internal',
     }
 }
 
